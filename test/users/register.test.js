276f877f4cbaa48679d19aded93d7263a7dd13ee
t-kurihara <takuya.kurihara00@gmail.com>
2018-05-05 23:36:48 +0900

    [change] extract common function for test

--- test/users/register.test.js
+++ test/users/register.test.js
@@ -1,70 +1,66 @@
 'use strict';
+const Util = require('../Util');
+
+const MAIL_REGEX = /^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$/;
+
+test('test', async () => {
+  const payload = {
+    id: '0000002',
+    user: {
+      mail: 'john.hoe@test.jp',
+      name: 'John Doe',
+    }
+  };
+
+  await Util.post('register', payload, response => {
+    const payload = JSON.parse(response.payload);
+
+    expect(response.statusCode).toBe(200);
+    expect(payload.id).toEqual(expect.stringMatching(/^[0-9]+$/));
+    expect(payload.user.mail).toEqual(expect.stringMatching(MAIL_REGEX));
+    expect(typeof payload.user.name).toBe('string');
+  });
+
+});
 
 test('correct params return 200', async () => {
-  const fastify = await (await require('../../server'))();
-
-  fastify.inject(
-    {
-      method: 'POST',
-      url: '/register',
-      payload: {
-        id: '0000002',
-        user: {
-          mail: 'john.hoe@test.jp',
-          name: 'John Doe',
-        },
-      }
+  const payload = {
+    id: '0000002',
+    user: {
+      mail: 'john.hoe@test.jp',
+      name: 'John Doe',
     },
-    response => {
-      const payload = JSON.parse(response.payload);
+  };
 
-      expect(response.statusCode).toBe(200);
-      expect(payload.id).toEqual(expect.stringMatching(/^[0-9]+$/));
-      expect(payload.user.mail).toEqual(expect.stringMatching(/^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$/));
-      expect(typeof payload.user.name).toBe('string');
-    }
-  );
+  await Util.post('/register', payload, response => {
+    const payload = JSON.parse(response.payload);
 
-  fastify.close();
+    expect(response.statusCode).toBe(200);
+    expect(payload.id).toEqual(expect.stringMatching(/^[0-9]+$/));
+    expect(payload.user.mail).toEqual(expect.stringMatching(MAIL_REGEX));
+    expect(typeof payload.user.name).toBe('string');
+  });
 });
 
 test('parameter-name brunk is invalid', async () => {
-  const fastify = await (await require('../../server'))();
-
-  fastify.inject(
-    {
-      method: 'POST',
-      url: '/register',
-      payload: {
-        id: '0000002',
-        user: {
-          mail: 'john.hoe@test.jp',
-        },
-      }
+  const payload = {
+    id: '0000002',
+    user: {
+      mail: 'john.hoe@test.jp',
     },
-    response => expect(response.statusCode).toBe(400)
-  );
+  };
 
-  fastify.close();
+  await Util.post('/register', payload, response => expect(response.statusCode).toBe(400));
 });
 
 test('parameter-id typeof number is invalid', async () => {
-  const fastify = await (await require('../../server'))();
-
-  fastify.inject(
-    {
-      method: 'POST',
-      url: '/register',
-      payload: {
-        id: 2,
-        user: {
-          mail: 'john.hoe@test.jp',
-          name: 'John Doe',
-        },
-      }
+  const payload = {
+    id: 'test',
+    user: {
+      mail: 'john.hoe@test.jp',
+      name: 'John Doe',
     },
-    response => expect(response.statusCode).toBe(400)
-  );
+  };
 
-  fastify.close();
+  await Util.post('/register', payload, response => expect(response.statusCode).toBe(400));
 });
