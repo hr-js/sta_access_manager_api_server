be9a9c8d95fe2b725cbede691d49de29ef7ff14f
t-kurihara <takuya.kurihara00@gmail.com>
2018-05-06 22:24:04 +0900

    [add] route "/register" invalid test

--- test/users/register.test.js
+++ test/users/register.test.js
@@ -1,27 +1,7 @@
 'use strict';
 const Util = require('../Util');
-
 const MAIL_REGEX = /^([a-zA-Z0-9])+([a-zA-Z0-9\\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\\._-]+)+$/;
 
-test('test', async () => {
-  const payload = {
-    id: '0000002',
-    user: {
-      mail: 'john.hoe@test.jp',
-      name: 'John Doe',
-    }
-  };
-
-  await Util.post('register', payload, response => {
-    const payload = JSON.parse(response.payload);
-
-    expect(response.statusCode).toBe(200);
-    expect(payload.id).toEqual(expect.stringMatching(/^[0-9]+$/));
-    expect(payload.user.mail).toEqual(expect.stringMatching(MAIL_REGEX));
-    expect(typeof payload.user.name).toBe('string');
-  });
-
-});
 
 test('correct params return 200', async () => {
   const payload = {
@@ -42,7 +22,7 @@ test('correct params return 200', async () => {
   });
 });
 
-test('parameter-name brunk is invalid', async () => {
+test('parameter-name is brunk return 400', async () => {
   const payload = {
     id: '0000002',
     user: {
@@ -53,11 +33,45 @@ test('parameter-name brunk is invalid', async () => {
   await Util.post('/register', payload, response => expect(response.statusCode).toBe(400));
 });
 
-test('parameter-id typeof number is invalid', async () => {
+test('parameter-id is brunk return 400', async () => {
+  const payload = {
+    user: {
+      mail: 'john.hoe@test.jp',
+      name: 'John Doe',
+    },
+  };
+
+  await Util.post('/register', payload, response => expect(response.statusCode).toBe(400));
+});
+
+test('parameter-id type isnot number return 400', async () => {
   const payload = {
     id: 'test',
     user: {
       mail: 'john.hoe@test.jp',
+      name: 'John Doe',
+    },
+  };
+
+  await Util.post('/register', payload, response => expect(response.statusCode).toBe(400));
+});
+
+test('parameter-mail is brunk return 400', async () => {
+  const payload = {
+    id: '0000002',
+    user: {
+      name: 'John Doe',
+    },
+  };
+
+  await Util.post('/register', payload, response => expect(response.statusCode).toBe(400));
+});
+
+test('parameter-mail is invalid pattern return 400', async () => {
+  const payload = {
+    id: '0000002',
+    user: {
+      mail: 'foo',
       name: 'John Doe',
     },
   };
