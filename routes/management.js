efa02526da71d328421f3d964ef594d527e0131d
Tchikawa <k10d2019@gmail.com>
2018-05-19 14:25:18 +0900

    ディレクトリ名修正。TODOに説明追加

--- routes/management.js
+++ routes/management.js
@@ -2,30 +2,39 @@ module.exports = async function(fastify, opts, next) {
   const {
     ACCESS_RCD_SCHEMA,
     LASTACCESS_RCD_SCHEMA,
-    AGGREGATE_SCHEMA
-  } = require('../assets/mgmt_schema');
+    AGGREGATE_SCHEMA,
+  } = require('../assets/mgmtSchema');
 
   const handler = (response, reply) => {
     reply.send(response);
   };
 
-  //TODO
-  const provider = require('./mgmt_provider')('localhost',9200);
+  //TODO providerが作成されたらパスを合わせる
+  const provider = require('./mgmtProvider')('localhost', 9200);
 
   fastify.get('/users', ACCESS_RCD_SCHEMA, async (request, reply) => {
-    //TODO
-    provider.getAccessRcd(request.date).then(response => handler(response, reply));
+    provider
+      .getAccessRcd(request.date)
+      .then(response => handler(response, reply));
   });
 
-  fastify.get('/users/recent', LASTACCESS_RCD_SCHEMA, async (request, reply) => {
-    //TODO
-    provider.getLastAcccessRcd().then(response => handler(response,reply));
-  });
+  fastify.get(
+    '/users/recent',
+    LASTACCESS_RCD_SCHEMA,
+    async (request, reply) => {
+      provider.getLastAcccessRcd().then(response => handler(response, reply));
+    }
+  );
 
-  fastify.get('/total/user/purpose', AGGREGATE_SCHEMA, async (request, reply) =>{
-    //TODO
-    provider.getAggregate(request.sort).then(response => handler(response,reply));
-  })
+  fastify.get(
+    '/total/user/purpose',
+    AGGREGATE_SCHEMA,
+    async (request, reply) => {
+      provider
+        .getAggregate(request.sort)
+        .then(response => handler(response, reply));
+    }
+  );
 
   next();
 };
