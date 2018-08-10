e67c0146b2eff7340bd084366a7a5f9aee4748a1
t-kurihara <takuya.kurihara00@gmail.com>
2018-08-10 23:13:19 +0900

    [add/wip] add swagger plugin.

--- app/controller/server.js
+++ app/controller/server.js
@@ -26,6 +26,37 @@ const getOpt = () => {
 const buildServer = async () => {
   const fastify = (await require('fastify'))(getOpt());
 
+  fastify.register(require('fastify-swagger'), {
+    routePrefix: '/documentation',
+    swagger: {
+      info: {
+        title: 'Test swagger',
+        description: 'testing the fastify swagger api',
+        version: '0.1.0'
+      },
+      externalDocs: {
+        url: 'https://swagger.io',
+        description: 'Find more info here'
+      },
+      host: 'localhost',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'user', description: 'User related end-points' },
+        { name: 'code', description: 'Code related end-points' }
+      ],
+      securityDefinitions: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'apiKey',
+          in: 'header'
+        }
+      }
+    },
+    exposeRoute: true
+  });
+
   fastify.register(await require('./users'), { logLevel: 'error' });
 
   fastify.register(require('./management'), { logLevel: 'error' });
@@ -35,7 +66,7 @@ const buildServer = async () => {
     next();
   });
 
-  await init();
+  // await init();
 
   return fastify;
 };
